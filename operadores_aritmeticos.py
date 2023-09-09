0969e6ed77702c2796b6d40d27e42e119649e252
DiogueraB <diogobatistaguimaraes10@gmail.com>
2023-09-09 15:13:58 -0300

    atividade sobre operadores aritmeticos

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,19 @@
+print ("---------------------------------------------")
+print ("-----------Operadores Aritméticos------------")
+print ("---------------------------------------------")
+
+A = 7
+B = 2
+C = 4 
+D = 14
+
+print (f"1. {A + B - C}")
+print (f"2. {A * B / C}")
+print (f"3. {C + 22 * D}")
+print (f"4. {A + B * C + D}")
+print (f"5. {(A+B) * (C+D)}")
+print (f"6. {(D/A) / (B/C)}")
+print (f"7. {10 ** A} ")
+print (f"8. {58 % A}")
+print (f"9. {(A + B + C * D) / (B - C * (B - C))}")
+print (f"10. {(A + B + C * D) / (B - C * (B - C)) + 10 ** A}")
