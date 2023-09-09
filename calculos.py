b9df78a05c3963086ff97cda5a8bb15019153bff
DiogueraB <diogobatistaguimaraes10@gmail.com>
2023-09-09 12:05:55 -0300

    Arquivo de teste de calculos

---
+++ calculos.py
@@ -0,0 +1,10 @@
+# Arquivo relacionado a testes de cálculos
+
+numero1 = 2 # variável do tipo inteiro
+numero2 = 13 # variável do tipo inteiro
+
+print (numero1 + numero2)
+
+resultado = (numero1 + numero2)
+print("O resultado é",resultado)
+print(f"O resultado é {resultado}")
