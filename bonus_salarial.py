9e3fe8b1df01f8131da3fee0978830b999704ff1
DiogueraB <diogobatistaguimaraes10@gmail.com>
2023-09-09 17:32:34 -0300

    programa que concede um bonus salarial

---
+++ bonus_salarial.py
@@ -0,0 +1,17 @@
+# Peça ao usuário para inserir seu salário e o tempo de serviço. Se o tempo de serviço
+# for superior a 5 anos, conceda um bônus de 5% ao salário.
+
+print ("--------------Bonus Salarial--------------")
+print ()
+
+salario = float(input ("Insira seu salário: "))
+tempo = int(input ("Qual é o seu tempo de serviço: "))
+
+print ()
+
+
+
+if tempo > 5:
+    print("Você recebeu um bônus de 5%")
+    print (f"Seu salário atual é de")
+
