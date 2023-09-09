03ed9d95d99e7f6bc9ddc1bafff413d83c85f8fc
DiogueraB <diogobatistaguimaraes10@gmail.com>
2023-09-09 12:58:33 -0300

    Programa para converter a temperatura

---
+++ temperatura.py
@@ -0,0 +1,17 @@
+print ("------------------------------------------")
+print ("---------Conversão de Temperatura---------")
+print ("------------------------------------------")
+
+celsius = float (input ("Qual a temperatura em Graus Celsius? "))
+
+print("--------------------------------------------")
+
+fahrenheit = (celsius*9/5) + 32
+
+print (f"A conversão de Celsius para Fahrenheit é: {fahrenheit}")
+
+print("--------------------------------------------")
+
+kelvin = (celsius + 273)
+
+print (f"A conversão de Celsius para Kelvin é: {kelvin}")
