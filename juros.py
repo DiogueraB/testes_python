301e4888b92b9cf58dfaeaae0688aefe19dd8f7a
DiogueraB <diogobatistaguimaraes10@gmail.com>
2023-09-09 12:58:22 -0300

    Programa para calcular juros

---
+++ juros.py
@@ -0,0 +1,15 @@
+print ("--------------------------------------------")
+print ("--------Calculadora de Juros Simples--------")
+print ("--------------------------------------------")
+
+valor = float(input("Qual o valor principal? "))
+jurosanual = float(input("Qual é o Juros Anual? "))
+tempo = int (input("Qual foi o período de Tempo em Anos ? "))
+
+juros = jurosanual/100
+
+print ("--------------------------------------------")
+
+montante = valor + (valor * juros * tempo)
+
+print (f"O montante é {montante}.")
