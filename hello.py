cd5128bd1dcfe4a42c5fdcb6e663d42af89ef11b
DiogueraB <diogobatistaguimaraes10@gmail.com>
2023-09-09 10:51:31 -0300

    Meu arquivo hello world

---
+++ hello.py
@@ -0,0 +1,9 @@
+print ('Hello World!')
+
+nome = "Diogo"
+profissao = "Programador"
+
+print (f"Este é o primeiro programa de {nome}")
+print ("Eu sou", profissao, "e estou aprendendo sobre Python")
+
+resposta = input ("Você está pronto para vir comigo (S/N)? ")
